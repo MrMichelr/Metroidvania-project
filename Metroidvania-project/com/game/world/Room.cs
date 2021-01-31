4e3c5722a399bf4ecf82f5735372b74460a62794
Michel Rodriguez <Me@mrmichelr.design>
2021-01-31 23:53:24 +0100

    WorldGeneration 2

--- Metroidvania-project/com/game/world/Room.cs
+++ Metroidvania-project/com/game/world/Room.cs
@@ -8,42 +8,75 @@ namespace com.game.world
         public int X, Y;
         public int pick, numberOfRooms;
 
+        public static int tileWidth = 12;
+        public static int tileHeight = 10;
+        public int[][] tileMap;
+
+        public Random percentTiles;
+        public bool Keep33, Keep50 = false;
+
         public Room()
         {
             numberOfRooms = 6;
-            
+            percentTiles = new Random();
+
+            tileMap = new int[tileWidth][];
+
+        }
+
+
+        public virtual void fillTileMap()
+        {
+            bool Flip = false;
+            bool HaveASecret = false;
+
+            if (percentTiles.Next(0,100) < 25) { HaveASecret = true; }
+            if (percentTiles.Next(0,100) < 33) { Keep33 = true; }
+            if (percentTiles.Next(0,100) < 50) { Keep50 = true; }
+            if (percentTiles.Next(0,100) > 50) { Flip = true; }
 
             switch (pick)
             {
                 case 0:
-                    Console.Write("0");
+                    TranslateTextToRoom(Flip, HaveASecret);
                     break;
 
                 case 1:
-                    Console.Write("1");
+                    TranslateTextToRoom(Flip, HaveASecret);
                     break;
-                    
+
                 case 2:
-                    Console.Write("2");
+                    TranslateTextToRoom(Flip, HaveASecret);
                     break;
-                    
+
                 case 3:
-                    Console.Write("3");
+                    TranslateTextToRoom(Flip, HaveASecret);
                     break;
-                    
+
                 case 4:
-                    Console.Write("4");
+                    TranslateTextToRoom(Flip, HaveASecret);
                     break;
-                    
+
                 case 5:
-                    Console.Write("5");
+                    Globals.debug("Placing firstroom (TYPE 5)");
+                    TranslateTextToRoom(Flip, HaveASecret);
                     break;
-                    
+
                 case 6:
-                    Console.Write("6");
+                    TranslateTextToRoom(Flip, HaveASecret);
                     break;
 
             }
+
+        }
+
+        public virtual void TranslateTextToRoom(bool _isFlip, bool _HasASecret)
+        {
+            Globals.debug("Room: " + pick + "-----------------------", ConsoleColor.Magenta);
+            Globals.debug("Flip ?: " + _isFlip, ConsoleColor.DarkMagenta);
+            Globals.debug("Have a secret: " + _HasASecret, ConsoleColor.DarkMagenta);
+
+            
         }
     }
 }
--- Metroidvania-project/com/game/world/WorldGenerator.cs
+++ Metroidvania-project/com/game/world/WorldGenerator.cs
@@ -5,24 +5,32 @@ namespace com.game.world
 {
     public class WorldGenerator
     {
-        public Room[,] rooms;
+        public static Room[,] rooms;
         public int[] tileMap;
-        public int rows, columns;
-        public static int myRandom;
+        public static int rows, columns;
         public Random pickARoom;
+
+        public int SpawnRoom;
 
         public int forceSunHeat;
         public int starGalaxy, starSystem, starID;
+
+        public static int worldSeed, worldStartedSeed;
 
 
         public WorldGenerator(int _forceSunHeat, int _starGalaxy, int _starSystem, int _starID)
         {
 
+            forceSunHeat = _forceSunHeat;
+            starGalaxy = _starGalaxy;
+            starSystem = _starSystem;
+            starID = _starID;
+
             Initialize();
 
             GenerateWorld();
 
-            LoadRooms();
+            //LoadRooms();
 
         }
 
@@ -34,11 +42,43 @@ namespace com.game.world
             columns = 3;
         }
 
-        public virtual void LoadRooms()
+        public virtual void GenerateWorld()
+        {
+            // Generate seed
+            int seed = starGalaxy * 640 + starSystem * 16 + starID;
+
+            worldSeed = seed;
+            worldStartedSeed = seed;
+
+            Globals.debug("PLANET #:" + seed);
+
+            GenerateWorldName();
+
+            GenerateGridRoom();
+
+            SetupNature();
+
+            SpawnRoom = pickARoom.Next(0, columns - 1);
+            //player position = -1, -1
+
+            DefineByRow(SpawnRoom);
+            GenerateRooms();
+            AssembleRooms();
+        }
+
+        public virtual void GenerateWorldName()
+        {
+
+        }
+
+        public virtual void SetupNature()
+        {
+
+        }
+
+        public virtual void GenerateGridRoom()
         {
             rooms = new Room[columns, rows];
-
-            Globals.debug("Room Picking -------------");
 
             for (int x = 0; x < columns; x++)
             {
@@ -48,27 +88,105 @@ namespace com.game.world
                     room.X = x;
                     room.Y = y;
 
-                    room.pick = pickARoom.Next(0, room.numberOfRooms);
-
-                    rooms[x,y] = room;
-
-                    Globals.debug(rooms[x, y].pick);
+                    rooms[x, y] = room;
                 }
             }
 
         }
 
-        public virtual void GenerateWorld()
+        public static void DefineByRow(int _spawnRoom)
+        {
+            bool firstRoom = true;
+            bool dropped = true;
+            int y = 0;
+
+            for (int x = 0; x < columns; x++)
+            {
+                rooms[x, y].pick = 6;
+
+                if(x == _spawnRoom)
+                {
+                    Globals.debug("First Room:" + x + " " + y);
+                }
+            }
+
+            y++;
+
+            int direction = -1;
+            int X = _spawnRoom;
+
+            while(y < rows)
+            {
+                rooms[X, y].pick = 1;
+
+                if (firstRoom)
+                {
+                    rooms[X, y].pick = 5;
+                    firstRoom = false;
+
+                    direction = getMyRandombySeed(4);
+                    if (X < 2) { direction = 2; }
+                    else if (X >= columns - 2) { direction = 0; }
+
+                }else if (dropped)
+                {
+                    dropped = false;
+                    rooms[X, y].pick = 3;
+                }
+
+
+
+                y++;
+            }
+
+            y--;
+
+            rooms[X, y].pick = 3;
+            //rooms[X, y].isLastRoom = true;
+
+
+            for (int x = 0; x < columns; x++)
+            {
+                for (int Y = 0; Y < rows; Y++)
+                {
+                    //for the rest
+                }
+            }
+
+        }
+
+        public virtual void GenerateRooms()
+        {
+            int roomID = 0;
+            foreach (var room in rooms)
+            {
+                room.fillTileMap();
+
+                if (room.X == 0)
+                {
+                    //Change the top rooms
+
+                    
+                }
+                
+                roomID++;
+            }
+
+        }
+
+        public virtual void AssembleRooms()
         {
 
         }
 
-        public static int getMyRandom(int _value)
+        public static int getMyRandombySeed(int value)
         {
-            int result = myRandom % _value;
-            int t = 1 + (myRandom ^ myRandom << 11);
-            myRandom = myRandom ^ myRandom >> 19 ^ t ^ t >> 8;
+            int result = worldSeed % value;
+            int t = 1 + (worldSeed ^ worldSeed << 11);
+            worldSeed = worldSeed ^ worldSeed >> 19 ^ t ^ t >> 8;
             return result;
         }
+
+
     }
 }
